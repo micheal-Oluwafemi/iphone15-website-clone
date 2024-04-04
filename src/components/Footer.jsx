32eede7cc22657a927fb114719e63d4b5d16ad74
micheal-Oluwafemi <raytech401@gmail.com>
2024-04-04 09:30:15 -1100

    Uploaded all Files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import { footerLinks } from "../constants";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className='py-5 sm:px-10 px-5'>
+      <div className='screen-max-width'>
+        <div>
+          <p className='font-semibold text-gray text-xs'>
+            More ways to shop:{" "}
+            <span className='underline text-blue pr-1'>
+              Find an Apple Store{" "}
+            </span>{" "}
+            or <span className='underline text-blue pr-1'> other retailer</span>{" "}
+            near you
+          </p>
+
+          <p className='font-semibold text-gray text-xs'>
+            Or call 0008000-040-3231
+          </p>
+        </div>
+
+        <div className='bg-neutral-700 my-5 h-[1px] w-full' />
+
+        <div className='flex md:flex-row flex-col md:text-center justify-between'>
+          <p className='font-semibold text-gray text-xs'>
+            Copyright &copy; {year} Apple Inc. All rights reserved.
+          </p>
+
+          <div className='flex '>
+            {footerLinks.map((link, i) => (
+              <p key={link} className='font-semibold text-gray text-xs'>
+                {link}{" "}
+                {i !== footerLinks.length - 1 && (
+                  <span className='mx-2'> | </span>
+                )}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f7b32d30400eb7d98ba92094e6f36f69@o4507029594112000.ingest.us.sentry.io/4507029607874560",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
