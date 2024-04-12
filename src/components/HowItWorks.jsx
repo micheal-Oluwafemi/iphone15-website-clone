b1151d139420c79c206966a2d1608fc62f73e0a0
micheal-Oluwafemi <raytech401@gmail.com>
2024-04-12 10:22:39 -1100

    Updated...

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -47,7 +47,7 @@ const HowItWorks = () => {
 
         <div className='mt-10 md:mt-20 mb-14'>
           <div className='relative h-full flex-center'>
-            <div className='overflow-x-hidden'>
+            <div className='overflow-x-hidden overflow-y-scroll'>
               <img
                 src={frameImg}
                 alt='frame'
